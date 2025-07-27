7cf04d53152fcf532b2a923e93ef0fc31b0d1b18
sangyeong01 <tkddud386@gmail.com>
2025-07-27 22:22:40 +0900

    week5: 31

--- 8_dp/31_gold_mine.py
+++ 8_dp/31_gold_mine.py
@@ -1,10 +1,10 @@
 T = int(input())
 
 for _ in range(T):
-    mine = []
-
     n, m = map(int, input().split())
     lines = list(map(int, input().split()))
+
+    mine = []
     for i in range(n):
         mine.append(lines[i * m : i * m + m])
 
@@ -12,20 +12,25 @@ for _ in range(T):
     for _ in range(n):
         dp.append([-1 for _ in range(m)])
 
-    # init
+    # initial setting
     for i in range(n):
         dp[i][0] = mine[i][0]
+
+    # general case
     for col in range(1, m):
         for row in range(n):
+            # left-up, left, left-down
             if row > 0 and row < n - 1:
                 dp[row][col] = (
                     max(dp[row - 1][col - 1], dp[row][col - 1], dp[row + 1][col - 1])
                     + mine[row][col]
                 )
+            # left-up, left
             elif row > 0:
                 dp[row][col] = (
                     max(dp[row - 1][col - 1], dp[row][col - 1]) + mine[row][col]
                 )
+            # left, left-down
             else:
                 dp[row][col] = (
                     max(dp[row][col - 1], dp[row + 1][col - 1]) + mine[row][col]
